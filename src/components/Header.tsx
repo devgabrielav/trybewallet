4e2eadeacf4aba440a37eb155fb3942caa4e904d
devgabrielav <gabrivicentini@icloud.com>
2023-08-30 18:11:11 -0300

    Requisito 2 completed!!

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,15 @@
-function Header() {
-  return <div>Heade</div>;
-}
+import { useSelector } from 'react-redux';
+import { GlobalStateType } from '../types';
 
+function Header() {
+  const { email } = useSelector((globalState: GlobalStateType) => globalState.user);
+
+  return (
+    <header>
+      <p data-testid="email-field">{email}</p>
+      <span data-testid="total-field">0</span>
+      <span data-testid="header-currency-field">BRL</span>
+    </header>
+  );
+}
 export default Header;
--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,15 @@ export type ActionType = {
   type: string,
   payload: string,
 };
+
+export type GlobalStateType = {
+  user: {
+    email: string, // string que armazena o e-mail da pessoa usuária
+  },
+  wallet: {
+    currencies: string[], // array de string
+    expenses: object[], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
+    editor: boolean, // valor booleano que indica se uma despesa está sendo editada
+    idToEdit: number, // valor numérico que armazena o id da despesa que está sendo editada
+  }
+};
